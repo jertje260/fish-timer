7f0ec11e3b8fc2e2b41f3ba716c4af606e1dcef0
Jeroen Broekhuizen <jeroen.broekhuizen@jedlix.com>
2019-09-23 21:27:13 +0200

    updated tests on getting last profile

--- test/WhenGettingPreviousDayForProfile.spec.ts
+++ test/WhenGettingPreviousDayForProfile.spec.ts
@@ -1,5 +1,74 @@
+import { ProfileSchedule } from "../src/Models/ProfileSchedule";
+import { Profile } from "../src/Models/Profile";
+import { DayOfWeek } from "../src/Models/Weekday";
+
 describe('When getting previous day for profile', () => {
-    it('Given same profile everyday, running on wednesday, should result on tuesday', () => {
-        
+    describe('Given same profile everyday', () => {
+
+        it('running on wednesday, should result on tuesday', () => {
+            const schedule = GivenSameSchedule();
+            const profile = GivenFirstProfile();
+            const result = schedule.GetPreviousDayForProfile(profile, new Date(2019, 8, 25));
+
+            expect(result.GetDayOfWeek())
+                .toBe(DayOfWeek.Tuesday);
+        });
+
+        it('running on monday, should result on sunday', () => {
+            const schedule = GivenSameSchedule();
+            const profile = GivenFirstProfile();
+            const result = schedule.GetPreviousDayForProfile(profile, new Date(2019, 8, 23));
+
+            expect(result.GetDayOfWeek())
+                .toBe(DayOfWeek.Sunday);
+        });
     });
+    describe('Given different profile everyday', () => {
+        it('running on wednesday, should result on monday', () => {
+            const schedule = GivenDifferentSchedule();
+            const profile = GivenFirstProfile();
+            const result = schedule.GetPreviousDayForProfile(profile, new Date(2019, 8, 25));
+
+            expect(result.GetDayOfWeek())
+                .toBe(DayOfWeek.Monday);
+        });
+
+        it('running on monday, should result on saturday', () => {
+            const schedule = GivenDifferentSchedule();
+            const profile = GivenFirstProfile();
+            const result = schedule.GetPreviousDayForProfile(profile, new Date(2019, 8, 25));
+
+            expect(result.GetDayOfWeek())
+                .toBe(DayOfWeek.Saturday);
+        });
+    });
+
+    describe('Given same profile last week', () => {
+        it('running on monday return last week', () => {
+            const schedule = GivenMondayOnlySchedule();
+            const profile = GivenFirstProfile();
+            const result = schedule.GetPreviousDayForProfile(profile, new Date(2019, 8, 23));
+
+            expect(result)
+                .toBe(new Date(2019, 8, 16));
+        });
+    });
+
 });
+
+
+function GivenSameSchedule(): ProfileSchedule {
+    return new ProfileSchedule(['1', '2', '3', '4', '5', '6', '7'], [0, 0, 0, 0, 0, 0, 0]);
+}
+
+function GivenDifferentSchedule(): ProfileSchedule {
+    return new ProfileSchedule(['1', '2', '3', '4', '5', '6', '7'], [0, 1, 0, 0, 1, 0, 1]);
+}
+
+function GivenMondayOnlySchedule(): ProfileSchedule {
+    return new ProfileSchedule(['1', '2', '3', '4', '5', '6', '7'], [0, 1, 1, 1, 1, 1, 1]);
+}
+
+function GivenFirstProfile(): Profile {
+    return new Profile(0, '', [], [], [], [], [], [], []);
+}
