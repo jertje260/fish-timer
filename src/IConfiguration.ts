cdb27caf7fc6099fa4e44719bcbb05e64571f81b
Jeroen Broekhuizen <jeroen.broekhuizen@jedlix.com>
2020-02-18 09:01:32 +0100

    extend configuration for profile configuration

--- src/IConfiguration.ts
+++ src/IConfiguration.ts
@@ -1,9 +1,19 @@
 export interface IConfiguration {
 	location: ILocation
 	smartControlIp: string
+	profileConfig: IProfilesConfig
 }
 
 export interface ILocation {
 	latitude: number,
 	longitude: number
+}
+
+export interface IProfilesConfig {
+	[key:number] : IProfileConfig
+}
+
+export interface IProfileConfig {
+	sunUpMatch: number,
+	sunDownMatch: number
 }
